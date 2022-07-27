bec7dff659473b1fa7b035df95143fba78da92b4
Waris Ninkaesorn <61102792+WarisNin@users.noreply.github.com>
2022-07-27 23:49:17 +0700

    First upload

---
+++ game.js
@@ -0,0 +1,78 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).on("keydown", function () {
+	if (!started) {
+		$("#level-title").text("Level " + level);
+		nextSequence();
+		started = true;
+	}
+});
+
+$(".btn").on("click", function () {
+	var userChosenColour = $(this).attr("id");
+	userClickedPattern.push(userChosenColour);
+
+	playSound(userChosenColour);
+	animatePress(userChosenColour);
+	checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+	userClickedPattern = [];
+
+	level++;
+	$("#level-title").text("Level " + level);
+
+	var randomNumber = Math.floor(Math.random() * 4);
+	var randomChosenColour = buttonColours[randomNumber];
+	gamePattern.push(randomChosenColour);
+
+	$("#" + randomChosenColour)
+		.fadeIn(100)
+		.fadeOut(100)
+		.fadeIn(100);
+	playSound(randomChosenColour);
+}
+
+function checkAnswer(currentLevel) {
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+		if (userClickedPattern.length === gamePattern.length) {
+			$("#level-title").text("Success");
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+	} else {
+		$("#level-title").text("Game Over, Press Any key to Restart");
+		$("body").addClass("game-over");
+		setTimeout(function () {
+			$("body").removeClass("game-over");
+		}, 200);
+		playSound("wrong");
+		startOver();
+	}
+}
+
+function playSound(name) {
+	var audio = new Audio("sounds/" + name + ".mp3");
+	audio.play();
+}
+
+function animatePress(currentColour) {
+	$("#" + currentColour).addClass("pressed");
+	setTimeout(function () {
+		$("#" + currentColour).removeClass("pressed");
+	}, 100);
+}
+
+function startOver() {
+	level = 0;
+	gamePattern = [];
+	started = false;
+}
